b5ceb97f0df236e7fc71e5c3491cccc306a963a4
StefvanderLinden <50927330+StefvanderLinden@users.noreply.github.com>
2026-03-12 13:52:36 +0100

    added swing visualisation

--- src/main/java/com/capgemini/gameoflife/Main.java
+++ src/main/java/com/capgemini/gameoflife/Main.java
@@ -1,14 +1,13 @@
 package com.capgemini.gameoflife;
 
 import com.capgemini.gameoflife.presentation.GameOfLifeCLI;
+import com.capgemini.gameoflife.presentation.SwingDisplay;
 import com.capgemini.gameoflife.testdata.TestData;
 
 public class Main {
     public static void main(String[] args) {
         var board = TestData.testboard;
-
-        System.out.println("hi");
-        var cli = new GameOfLifeCLI();
-        cli.show(board);
+        var gameDisplay = new SwingDisplay();
+        gameDisplay.show(board);
     }
 }
